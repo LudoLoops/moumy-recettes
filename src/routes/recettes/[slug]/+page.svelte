285883d18e3c09cf21a09395753d67dbb2a3fb78
Ludo Loops <ludoloops@pm.me>
2026-04-17 23:45:34 +0200

    fix: add closing quote to Moumy's notes

--- src/routes/recettes/[slug]/+page.svelte
+++ src/routes/recettes/[slug]/+page.svelte
@@ -296,6 +296,13 @@
 						<span>Notes de Moumy</span>
 					</div>
 					<p class="pl-4">{recipe.notes}</p>
+					<span
+						class="bottom-2 right-3 text-5xl absolute leading-none opacity-30"
+						style="color: var(--color-gold); font-family: var(--font-title)"
+						aria-hidden="true"
+					>
+						{'\u201D'}
+					</span>
 				</div>
 			{/if}
 
